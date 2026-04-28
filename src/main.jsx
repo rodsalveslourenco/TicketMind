81fa2c6b083750de9dbe35b0ace08594e87e77de
Rodrigo Alves <contato@systemind.com.br>
2026-04-28 15:15:14 -0300

    Fix refresh routing with hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
 import { AppDataProvider } from "./data/AppDataContext";
@@ -8,12 +8,12 @@ import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <AppDataProvider>
           <App />
         </AppDataProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );
